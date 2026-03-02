ee907c2725377c1e8463d85e0f4c6eccf21212a9
tqaimkhani <tqaimkhani8@gmail.com>
2026-03-02 07:18:53 +0000

    New Data

--- basic1/Marksheet.js
+++ basic1/Marksheet.js
@@ -2,4 +2,34 @@ let name="Tahir Ammar";
 let fatherName="Muhammad Kaleen Khan";
 let surname="Qaim Khani";
 let District="HYD";
-console.log(name,fatherName,surname,District)
+
+let English = 95;
+let Urdu = 95;
+let Chemistry = 65;
+let science = 85;
+let totalmarks = English + Urdu + Chemistry + science;
+
+let percentage = (totalmarks/400)* 100;
+
+
+console.log("---------MARKSHEET----------")
+console.log("name : ", name );
+
+console.log("nameFather : ",fatherName );
+
+console.log("surname : ", surname );
+
+console.log("District : ", District );
+
+
+
+console.log(" English : ", English );
+console.log("Urdu : ", Urdu );
+console.log("Chemistry : ", Chemistry);
+console.log("science : ", science);
+
+console.log("totalmarks : ", totalmarks);
+console.log("percentage :", percentage);
+
+let grade = "A+"
+console.log(grade)
